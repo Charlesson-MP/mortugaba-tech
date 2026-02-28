48d969b78426f0f6544f4d3ae051072ce0d35b79
Charlesson Mendes Pereira <86690606+Charlesson-MP@users.noreply.github.com>
2026-02-28 11:54:22 -0300

    feat: Set up core Next.js application with a new home page, header component, and root layout. (#4)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Header } from "./components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased relative`}>
+        <Header />
+        <main>{children}</main>
       </body>
     </html>
   );
